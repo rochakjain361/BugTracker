81a199ff40180d86c7cb599af4d1282714f2aa99
Rochak Jain <rochak_r@ec.iitr.ac.in>
2020-05-10 19:11:45 +0530

    Views Updated

--- BugTracker/views.py
+++ BugTracker/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from BugTracker.serializers import *
 from BugTracker.models import *
 from BugTracker.permissions import *
+from rest_framework.response import Response
+from django.http import Http404
+from rest_framework.decorators import action, permission_classes
 
 # Create your views here.
 
@@ -21,8 +24,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
             except KeyError:
                 return Project.objects.all()
     queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAdminOrProjectCreator, IsTeamMemberOrAdmin]
+    serializer_class = ProjectSerializer 
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
@@ -33,19 +35,73 @@ class ProjectViewSet(viewsets.ModelViewSet):
         else:
             return ProjectEditSerializers
 
+    @action(methods=['get', ], detail=True, url_path='issues', url_name='issues')
+    def get_issues(self, requets, pk):
+        try:
+            issues_list = Issues.objects.filter(project=pk)
+        except KeyError:
+            return Response({'Empty': 'No Issues for this project yet'}, status = status.HTTP_204_NO_CONTENT)
+
+        ser = IssueSerializer(issues_list, many=True)
+        return Response(ser.data)
+
+    @action(methods=['get', ], detail=True, url_path='members', url_name='members')
+    def get_team_members(self, request, pk):
+        project = Project.objects.get(pk = pk)
+        members_list = project.members
+
+        ser = AppUserSerializer(members_list, many=True)
+        return Response
+
+    @action(methods=['get', ], detail=True, url_path='creator', url_name='creator')
+    def get_creator(self, request, pk):
+        project = Project.objects.get(pk = pk)
+        creator = project.creator
+
+        ser = UserSerializer(creator)
+        return Response(ser.data)
+
+    @permission_classes(IsAdminOrProjectCreator)
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+        except Http404:
+            pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class IssuesViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         try:
-            return Issues.objects.filter(assigned_to=self.kwargs['assigned_to_pk'])
-        except KeyError:
+            return Issues.objects.filter(project=self.kwargs['project_pk'])
+        except:
             try:
-                return Issues.objects.filter(reported_by=self.kwargs['reported_by_pk'])
+                return Issues.objects.filter(assigned_to=self.kwargs['assigned_to_pk'])
             except KeyError:
-                return Issues.objects.all()
-
+                try:
+                    return Issues.objects.filter(reported_by=self.kwargs['reported_by_pk'])
+                except KeyError:
+                    return Issues.objects.all()
     queryset = Issues.objects.all()
     serializer_class = IssueSerializer
-    permission_classes = [IsAdminOrReadOnly]
+
+    @action(methods=['patch', ], detail=True, url_path='assign', url_name='assign')
+    @permission_classes([IsTeamMemberOrAdmin, IsAdminOrProjectCreator])
+    def assign_issue(self, request, pk):
+        assigned_to = self.request.query_params.get('assigned_to')
+        issue = Issues.objects.get(pk=pk)
+        user = AppUser.objects.get(pk=assigned_to)
+
+        if user in issue.project.members.all():
+            ser = IssueSerializer(issue, data={'assigned_to': assign_to}, partial=True)
+
+            if ser.is_valid():
+                ser.save()
+            return Response({'status': 'Assignment Successful'}, status=status.HTTP_202_ACCEPTED)
+
+        else:
+            return Response({'Error': 'User not a team member'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
